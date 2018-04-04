e7d8463a2bc64cd557e5d708556fe22ac971809d
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2018-04-04 10:52:59 +0200

    Add `setup.py` and library

--- runnable_test.py
+++ runnable_test.py
@@ -1,2 +1,5 @@
+from mylib.adder import add_one
+
 if __name__ == '__main__':
+    assert add_one(3) == 4
     print 'All good, tests are passing!'
---
+++ setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='circleci-python-test-repo',
+    packages=['mylib'],
+    include_package_data=True,
+)
